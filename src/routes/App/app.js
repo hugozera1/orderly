d16bdd56095619d04f5c865b0dd373009803dc15
Arthur Cesar <arthurcesarmarcelino@gmail.com>
2024-09-04 22:05:59 -0300

    initializing api crud

--- src/routes/App/app.js
+++ src/routes/App/app.js
@@ -1,11 +1,48 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import './app.css'
 import MainHeader from "../../Components/mainHeader";
 
+import axios from "axios"
+
 function App() {
+
+  const [posts, setPosts] = useState([])
+
+  useEffect(() =>{
+    axios.get("https://my-json-server.typicode.com/arthuracmm/ecommerly/posts")
+    .then((res) => {
+      setPosts(res.data)
+    })
+    .catch(() => {
+      console.log('Deu errado')
+    })
+  }, [])
+
   return (
-    <div className="App">
+    <div>
       <MainHeader />
+
+      <main>
+          <div className="cards">
+            {posts.map((post, key) =>{
+                  return (
+                    <div className="card">
+                        <header>
+                          <h2>{post.title}</h2>
+                        </header>
+
+                        <p>{post.description}</p>
+                        <p>{post.value}</p>
+
+                        <div className="btns">
+                        
+                        </div>
+                    </div>
+                  )
+            })}
+            
+          </div>
+      </main>
     </div>
   );
 }
